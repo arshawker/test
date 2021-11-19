9786276205697d3ce4a23d16d688a5849c6c9443
Arslan <arslan93niyazov@gmail.com>
2021-11-19 16:35:49 +0100

    First test

---
+++ js/script.js
@@ -0,0 +1,55 @@
+'use strict';
+
+// alert('Hello');
+
+// const result = confirm('Are you here?');
+// console.log(result);
+
+// const answer = prompt('how old are you?', '18');
+// console.log(typeof(answer));
+
+// const answers = [];
+
+// answers[0] = prompt('name?', '');
+// answers[1] = prompt('surname?', '');
+// answers[3] = prompt('age?', '');
+
+// console.log(typeof(answers));
+
+// Интерполяция 
+// const category = 'toys';
+
+// console.log(`htttps://someurl.com/${category}/10`);
+
+// const user = 'Arslan';
+
+// alert(`Hello, ${user}`);
+
+// Операторы JS
+
+let incr = 10,
+    decr = 10;
+incr++;    //постфиксная форма записи
+decr--;    //постфиксная форма записи
+
+++incr;    //префиксная форма записи
+--decr;    //префиксная форма записи
+
+// console.log(incr);
+// console.log(decr);
+
+console.log(++incr);
+console.log(--decr);
+
+
+console.log(2*4 === 8);
+
+
+// Логические операторы
+// && // И
+// || // ИЛИ
+
+const isChecked = true,
+      isClose = false;
+// console.log(isChecked && isClose);
+console.log(isChecked || isClose);
